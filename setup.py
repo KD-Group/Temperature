8a54adcfc93c36df79f005a838bc50c25e81ffe0
WingC <1018957763@qq.com>
2019-02-08 19:14:52 +0800

    update to version v0.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ temperature_module = Extension('_temperature', sources=['temperature/temp_wrap.c
                                libraries=['Temperature'])
 
 setup(name='temperature',
-      version='0.5',
+      version='0.6',
       author="SF Zhou WingC",
       author_email="1018957763@qq.com",
       url="https://gitlab.com/KD-Group/temperature",
