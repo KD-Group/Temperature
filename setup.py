2d153a5728d2127d5df3572b50a44d4ac13110b6
Jefung <jefung865424525@gmail.com>
2019-07-15 22:18:59 +0800

    update version to 1.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup, Extension
 
 
 setup(name='temperature',
-      version='1.4',
+      version='1.6',
       author="WingC, SF Zhou",
       author_email="1018957763@qq.com",
       url="https://gitlab.com/KD-Group/temperature",
